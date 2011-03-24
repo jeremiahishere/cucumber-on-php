beabedb7f03eca44b1e2402505f0c4897220cd57
jeremiahishere <jeremiah@cloudspace.com>
2011-03-24 17:49:17 -0400

    continued working on new env/database setup.  See todos for more work

--- public/features/support/env.rb
+++ public/features/support/env.rb
@@ -20,95 +20,64 @@ Capybara.default_driver = :mechanize
 require 'test/unit/assertions'
 World(Test::Unit::Assertions)
 
-# Helper method for running shell commands
-def run(command, message = nil, verbose = true)
-  if verbose then
-    puts "#{message}"
-    puts command
-    result = `#{command}`
-    puts result
-    return result
-  else
-    `#{command}`
-  end
-end
-
 # Be very careful running this on the production server
 # The location of the current database connection file 
 @database_config_path = File.dirname(__FILE__)+"/../../includes/conn.php"
 @original_database_config = File.read(@database_config_path)
 
-# mysql doesn't automatically link on osx.
-# Change as necessary
-if RUBY_PLATFORM.downcase.include?('darwin')
-  @@mysql = "/usr/local/mysql-5.1.53-osx10.6-x86_64/bin/mysql" 
-else
-  @@mysql = "mysql" 
-end
-
 # mysql user with permissions to create database and grant permissions
 # these have the -u and -p arguments because you get additional dialogues if the mysql root password is blank
-@@mysql_admin_user = "-u" + "root"
-@@mysql_admin_password = "-p" + "password"
+@default_mysql_admin_user = "root"
+@default_mysql_admin_password = "password"
 
-# database location and name
-@@test_database_location = "localhost"
-@@test_database_name = "yoursitehere_test_db"
+# read the database information
+@databases = YAML.load(File.open('database.yml').read)
+# iterate over the databases, drop, create and seed, build the mysql_connect statements as well
+mysql_connect_output = ""
+@databases.each do |db|
+  admin_user = "-u" + db['admin_user'] ? db['admin_user'] : @default_mysql_admin_user
+  admin_pass = db['admin_passowrd'] ? db['admin_password'] : @default_mysql_admin_password
+  admin_pass = "-p" + admin_pass unless admin_pass.blank?
+  admin_user_info = admin_user + " " admin_pass
 
-# mysql user used in the conn file
-@@test_database_username = "root"
-@@test_database_password = "password"
+  schema_location = File.dirname(__FILE__) + "db_schemas/" + db['schema']
+  # drop, create, give permissions, seed
+  %x{echo \"DROP DATABASE #{db['name']};\" | mysql #{db['location']} #{admin_user_info};} 
+  %x{echo \"CREATE DATABASE #{db['name']};\" | mysql #{db['location']} #{admin_user_info};"}
+  %x{echo \"GRANT ALL PRIVILEGES ON #{db['name']}.* TO #{db['user']}@#{db['location']} IDENTIFIED BY '#{db['password']}'\" | mysql #{db['location']} #{admin_user_info}"}
+  %x{mysql -u#{db['user']} -p#{db['passowrd']} #{db['name']} < \"#{schema_location}\";"}
 
-# Path for the db dump used to seed the tests
-@path_to_seed_data = File.dirname(__FILE__)+"./db/dumps/yoursitehere_db_dump.sql"
+  mysql_connect_output += "$#{db['var_name']} = mysql_connect(\"#{db['location']}\", \"#{db['user']}\", \"#{db['password']}\");\nmysql_selectdb(\"#{db['name']}\", $#{db['var_name']});\n"
+end
 
 # Overwrite the conn file with the new database and authentication 
 File.open(@database_config_path, "w") do |file|
   file.puts "
   <?php
   $env = \"test\";
-  $MAIN_CONN = mysql_connect(\"#{@@test_database_location}\",\"#{@@test_database_username}\",\"#{@@test_database_password}\");
-  mysql_selectdb(\"#{@@test_database_name}\", $MAIN_CONN);
+  #{mysql_connect_output}
   ?>
   "
 end
 
-# Optional check to see if test database exists, if not create the user p
-# could be useful if drop, create, seed takes too long
-# unless(run("echo \"SHOW DATABASES;\" | mysql -u #{@@test_database_username} --password=#{@@test_database_password} 2>&1").match @@test_database_name ) then
-
-# drop the test database
-run("echo \"DROP DATABASE #{@@test_database_name};\" | #{@@mysql} #{@@mysql_admin_user} #{@@mysql_admin_password};", "Dropping test database")
-# add the test database
-run("echo \"CREATE DATABASE #{@@test_database_name};\" | #{@@mysql} #{@@mysql_admin_user} #{@@mysql_admin_password};", "Creating test database")
-# add privileges to the test database
-run("echo \"GRANT ALL PRIVILEGES ON #{@@test_database_name}.* TO #{@@test_database_username}@#{@@test_database_location} IDENTIFIED BY '#{@@test_database_password}'\" | #{@@mysql} #{@@mysql_admin_user} #{@@mysql_admin_password}", "Adding test user privileges to database")
-# seed test database
-run("#{@@mysql} -u#{@@test_database_username} -p#{@@test_database_password} #{@@test_database_name} < \"#{@path_to_seed_data}\";", "Seeding database")
-
-# setup display for selenium tests using xvfb
-# puts "Cucumber test with Xvfb and firefox"
-ENV['DISPLAY'] = ":99"
-run("Xvfb :99 -ac -screen 0 1024x768x16 2>/dev/null >/dev/null &", "Setup xvfb on display 99")
-run("firefox --display=:99 2>/dev/null >/dev/null &", "Launch firefox on display 99")
-
-
+def drop_databases
+  admin_user = "-u" + db['admin_user'] ? db['admin_user'] : @default_mysql_admin_user
+  admin_pass = db['admin_passowrd'] ? db['admin_password'] : @default_mysql_admin_password
+  admin_pass = "-p" + admin_pass unless admin_pass.blank?
+  admin_user_info = admin_user + " " admin_pass
+  @databases.each do |db|
+    %x{echo \"DROP DATABASE #{db['name']};\" | mysql #{db['location']} #{admin_user_info};"}
+  end
+end
 
 # Switch back to whatever was loaded originally
 def rollback_to_seed_data
   File.open(@database_config_path, "w") do |file|
     file.puts @original_database_config
   end
-
-  run("echo \"DROP DATABASE #{@@test_database_name};\" | #{@@mysql} #{@@mysql_admin_user} #{@@mysql_admin_password};", "Dropping test database")
-
-  run("killall Xvfb")
-  run("killall firefox")
-
-  # puts "Cucumber finished, Resetting test database"
-  # run "mysql -u #{@@test_database_username} --password=#{@@test_database_password} #{@@test_database_name} < #{@path_to_seed_data}"
 end
 
 at_exit do
   rollback_to_seed_data
+  drop_databases
 end
