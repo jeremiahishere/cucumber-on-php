1c8487eac12b812de2fa3ae9d5aa309cb4fe9bcb
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-06-16 13:22:30 -0400

    When using the after scenario database hook, do not try to delete records out of views.

--- public/features/support/hooks.rb
+++ public/features/support/hooks.rb
@@ -4,7 +4,7 @@ end
 
 After do |scenario|
   @@dbm.databases.each do |db|
-    @@dbm.query("show tables", db).each do |t|
+    @@dbm.query("show tables where Table_Type='BASE TABLE'", db).each do |t|
       # tables are in a hash that returns an array when you get first
       # no idea why
       table = t.first[1]
